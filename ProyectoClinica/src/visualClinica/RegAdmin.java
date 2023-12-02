29fc3f77fdbe600c5ae38d30aad08ebab8f0aee4
amber <amber@DESKTOP-LOBE7VJ>
2023-12-02 12:15:08 -0400

    Update visual part

--- ProyectoClinica/src/visualClinica/RegAdmin.java
+++ ProyectoClinica/src/visualClinica/RegAdmin.java
@@ -10,6 +10,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.border.BevelBorder;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
 public class RegAdmin extends JDialog {
 
@@ -35,72 +44,98 @@ public class RegAdmin extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegAdmin() {
-		setBounds(100, 100, 351, 312);
+		setResizable(false);
+		setTitle("Registrar Administrador");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RegAdmin.class.getResource("/imagen/MnUsu.png")));
+		setBounds(100, 100, 410, 461);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(SystemColor.text);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		{
 			JPanel panel = new JPanel();
+			panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+			panel.setBackground(SystemColor.text);
 			contentPanel.add(panel, BorderLayout.CENTER);
 			panel.setLayout(null);
 			{
 				JPanel panel_1 = new JPanel();
-				panel_1.setBorder(new TitledBorder(null, "Informacion general", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-				panel_1.setBounds(12, 13, 300, 205);
+				panel_1.setBackground(SystemColor.text);
+				panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informacion general", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 102, 204)));
+				panel_1.setBounds(12, 13, 373, 343);
 				panel.add(panel_1);
 				panel_1.setLayout(null);
 				{
-					JLabel lblNewLabel = new JLabel("Usuario:");
-					lblNewLabel.setBounds(12, 28, 56, 16);
-					panel_1.add(lblNewLabel);
-				}
-				{
 					txtUser = new JTextField();
-					txtUser.setBounds(12, 46, 259, 19);
+					txtUser.setBounds(25, 74, 93, 22);
 					panel_1.add(txtUser);
 					txtUser.setColumns(10);
 				}
 				{
-					JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a:");
-					lblNewLabel_1.setBounds(12, 90, 95, 16);
-					panel_1.add(lblNewLabel_1);
-				}
-				{
 					txtPass = new JTextField();
-					txtPass.setBounds(12, 105, 259, 22);
+					txtPass.setBounds(25, 251, 136, 22);
 					panel_1.add(txtPass);
 					txtPass.setColumns(10);
 				}
 				{
-					JLabel lblNewLabel_2 = new JLabel("Confirmar contrase\u00F1a:");
-					lblNewLabel_2.setBounds(12, 152, 136, 16);
-					panel_1.add(lblNewLabel_2);
-				}
-				{
 					txtPassC = new JTextField();
 					txtPassC.setColumns(10);
-					txtPassC.setBounds(12, 168, 259, 22);
+					txtPassC.setBounds(199, 251, 143, 22);
 					panel_1.add(txtPassC);
 				}
+				
+				JLabel lblUsuario = new JLabel("Usuario:");
+				lblUsuario.setForeground(new Color(0, 102, 204));
+				lblUsuario.setFont(new Font("Segoe UI", Font.BOLD, 15));
+				lblUsuario.setBounds(25, 35, 59, 26);
+				panel_1.add(lblUsuario);
+				
+				JLabel lblContraseaNueva = new JLabel("Contrase\u00F1a nueva:");
+				lblContraseaNueva.setForeground(new Color(0, 102, 204));
+				lblContraseaNueva.setFont(new Font("Segoe UI", Font.BOLD, 15));
+				lblContraseaNueva.setBounds(18, 204, 143, 22);
+				panel_1.add(lblContraseaNueva);
+				
+				JLabel lblConfirmarContrasea = new JLabel("Confirmar contrase\u00F1a:");
+				lblConfirmarContrasea.setForeground(new Color(0, 102, 204));
+				lblConfirmarContrasea.setFont(new Font("Segoe UI", Font.BOLD, 15));
+				lblConfirmarContrasea.setBounds(194, 204, 179, 22);
+				panel_1.add(lblConfirmarContrasea);
+				
+				JLabel lblNewLabel = new JLabel("");
+				lblNewLabel.setIcon(new ImageIcon(RegAdmin.class.getResource("/imagen/icnRegad.png")));
+				lblNewLabel.setBounds(183, 38, 150, 116);
+				panel_1.add(lblNewLabel);
 			}
 		}
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton regButton = new JButton("Registrar");
+				regButton.setBackground(Color.WHITE);
+				regButton.setForeground(new Color(0, 102, 204));
+				regButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
 				regButton.setActionCommand("OK");
 				buttonPane.add(regButton);
 				getRootPane().setDefaultButton(regButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setBackground(SystemColor.text);
+				cancelButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				cancelButton.setForeground(new Color(0, 102, 204));
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
--- ProyectoClinica/src/visualClinica/RegDoctor.java
+++ ProyectoClinica/src/visualClinica/RegDoctor.java
@@ -11,6 +11,16 @@ import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.border.BevelBorder;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class RegDoctor extends JDialog {
 
@@ -24,6 +34,7 @@ public class RegDoctor extends JDialog {
 	private JTextField txtUser;
 	private JTextField txtPassword;
 	private JTextField txtPasswordC;
+	private JTextField txtDire;
 
 	/**
 	 * Launch the application.
@@ -42,33 +53,46 @@ public class RegDoctor extends JDialog {
 	 * Create the dialog.
 	 */
 	public RegDoctor() {
-		setBounds(100, 100, 561, 514);
+		setTitle("Registrar Doctor");
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RegDoctor.class.getResource("/imagen/MnDoct.png")));
+		setBackground(SystemColor.text);
+		setBounds(100, 100, 513, 606);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(SystemColor.text);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		{
 			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel.setBackground(SystemColor.text);
 			contentPanel.add(panel);
 			panel.setLayout(null);
 			{
 				JPanel panel_general = new JPanel();
-				panel_general.setBorder(new TitledBorder(null, "Informacion general", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-				panel_general.setBounds(12, 13, 516, 253);
+				panel_general.setBackground(SystemColor.inactiveCaption);
+				panel_general.setFont(new Font("Segoe UI", Font.BOLD, 13));
+				panel_general.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informaci\u00F3n general", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 102, 204)));
+				panel_general.setBounds(0, 13, 497, 308);
 				panel.add(panel_general);
 				panel_general.setLayout(null);
 				
 				JLabel lblNewLabel = new JLabel("Nombre:");
-				lblNewLabel.setBounds(231, 34, 56, 16);
+				lblNewLabel.setForeground(new Color(0, 0, 153));
+				lblNewLabel.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel.setBounds(231, 34, 81, 16);
 				panel_general.add(lblNewLabel);
 				
 				txtNombre = new JTextField();
-				txtNombre.setBounds(309, 31, 186, 22);
+				txtNombre.setBounds(319, 31, 166, 22);
 				panel_general.add(txtNombre);
 				txtNombre.setColumns(10);
 				
-				JLabel lblNewLabel_1 = new JLabel("Cedula: ");
-				lblNewLabel_1.setBounds(12, 34, 56, 16);
+				JLabel lblNewLabel_1 = new JLabel("C\u00E9dula: ");
+				lblNewLabel_1.setForeground(new Color(0, 0, 153));
+				lblNewLabel_1.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_1.setBounds(12, 34, 81, 16);
 				panel_general.add(lblNewLabel_1);
 				
 				txtCedula = new JTextField();
@@ -76,58 +100,71 @@ public class RegDoctor extends JDialog {
 				panel_general.add(txtCedula);
 				txtCedula.setColumns(10);
 				
-				JLabel lblNewLabel_2 = new JLabel("Telefono:");
-				lblNewLabel_2.setBounds(12, 65, 56, 16);
+				JLabel lblNewLabel_2 = new JLabel("Tel\u00E9fono:");
+				lblNewLabel_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_2.setForeground(new Color(0, 0, 153));
+				lblNewLabel_2.setBounds(12, 69, 81, 16);
 				panel_general.add(lblNewLabel_2);
 				
 				txtTelefono = new JTextField();
-				txtTelefono.setBounds(78, 62, 140, 22);
+				txtTelefono.setBounds(76, 66, 140, 22);
 				panel_general.add(txtTelefono);
 				txtTelefono.setColumns(10);
 				
 				JLabel lblNewLabel_3 = new JLabel("Especialidad:");
-				lblNewLabel_3.setBounds(227, 65, 81, 16);
+				lblNewLabel_3.setForeground(new Color(0, 0, 153));
+				lblNewLabel_3.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_3.setBounds(231, 69, 81, 16);
 				panel_general.add(lblNewLabel_3);
 				
 				txtEspecialidad = new JTextField();
-				txtEspecialidad.setBounds(309, 66, 186, 22);
+				txtEspecialidad.setBounds(319, 66, 166, 22);
 				panel_general.add(txtEspecialidad);
 				txtEspecialidad.setColumns(10);
 				
-				JRadioButton rdbtnMasculino = new JRadioButton("Masculino");
-				rdbtnMasculino.setBounds(91, 90, 127, 25);
-				panel_general.add(rdbtnMasculino);
-				
-				JRadioButton rdbtnFemenino = new JRadioButton("Femenino");
-				rdbtnFemenino.setBounds(261, 90, 114, 25);
-				panel_general.add(rdbtnFemenino);
-				
 				JLabel lblNewLabel_4 = new JLabel("Sexo:");
-				lblNewLabel_4.setBounds(12, 94, 56, 16);
+				lblNewLabel_4.setForeground(new Color(0, 0, 153));
+				lblNewLabel_4.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_4.setBounds(12, 104, 81, 16);
 				panel_general.add(lblNewLabel_4);
 				
 				JLabel lblNewLabel_5 = new JLabel("Correo Electronico:");
-				lblNewLabel_5.setBounds(12, 124, 138, 16);
+				lblNewLabel_5.setForeground(new Color(0, 0, 153));
+				lblNewLabel_5.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_5.setBounds(12, 158, 138, 16);
 				panel_general.add(lblNewLabel_5);
 				
 				txtCorreo = new JTextField();
-				txtCorreo.setBounds(139, 124, 297, 22);
+				txtCorreo.setBounds(135, 155, 337, 22);
 				panel_general.add(txtCorreo);
 				txtCorreo.setColumns(10);
 				
 				JLabel lblNewLabel_6 = new JLabel("Direccion:");
-				lblNewLabel_6.setBounds(12, 153, 81, 16);
+				lblNewLabel_6.setForeground(new Color(0, 0, 153));
+				lblNewLabel_6.setFont(new Font("Segoe UI Black", Font.PLAIN, 13));
+				lblNewLabel_6.setBounds(231, 104, 81, 16);
 				panel_general.add(lblNewLabel_6);
 				
 				txtDireccion = new JTextField();
-				txtDireccion.setBounds(12, 168, 495, 72);
+				txtDireccion.setBounds(29, 223, 237, 72);
 				panel_general.add(txtDireccion);
 				txtDireccion.setColumns(10);
+				
+				JComboBox cbxSexo = new JComboBox();
+				cbxSexo.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "M", "F"}));
+				cbxSexo.setBounds(78, 101, 138, 22);
+				panel_general.add(cbxSexo);
+				
+				txtDire = new JTextField();
+				txtDire.setColumns(10);
+				txtDire.setBounds(319, 101, 166, 22);
+				panel_general.add(txtDire);
 			}
 			
 			JPanel panel_1 = new JPanel();
+			panel_1.setBackground(SystemColor.inactiveCaption);
 			panel_1.setBorder(new TitledBorder(null, "Usuario", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel_1.setBounds(12, 279, 516, 130);
+			panel_1.setBounds(0, 361, 516, 130);
 			panel.add(panel_1);
 			panel_1.setLayout(null);
 			
@@ -160,6 +197,7 @@ public class RegDoctor extends JDialog {
 		}
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
@@ -170,6 +208,11 @@ public class RegDoctor extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
